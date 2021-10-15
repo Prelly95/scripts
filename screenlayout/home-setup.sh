0e3d189cdcb8adbc9c183919d89edc8879b71ec3
Patrick Prell <pat@mecatron.localdomain>
2021-10-15 15:07:54 +1100

    always turn laptop screen on

--- screenlayout/home-setup.sh
+++ screenlayout/home-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xrandr \
 --output $1 --mode 3840x2160 --primary \
---output $2 --primary --mode 3840x2160 --right-of $1
+--output $2 --primary --mode 3840x2160 -r 60 --right-of $1
 
 notify-send "Display: HOME SETUP"

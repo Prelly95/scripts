95bebe1750faf02487e1f9a948332a6f3bb2ebe5
Patrick Prell <prelly95@gmail.com>
2021-10-15 16:08:08 +1100

    monitor position fix

--- screenlayout/work-setup.sh
+++ screenlayout/work-setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 xrandr \
---output $1  --mode 3840x2160 -r 60 --pos 0x1000 --crtc 1
+--output $1  --mode 3840x2160 -r 60 --crtc 1
 
 xrandr \
---output $2 --mode 3840x2160 -r 30 --pos 3840x1000 --rotate normal  --primary --crtc 0 \
---output $3 --mode 3840x2160 -r 30 --pos 7680x0 --rotate left --crtc 2
+--output $2 --mode 3840x2160 -r 30 --crtc 0 \
+--output $3 --mode 3840x2160 -r 30 --crtc 2
 
+xrandr --output eDP-1-1 --pos 0x0 \
+--output DP-1-1-1 --pos 3840x0 --rotate normal  --primary \
+--output DP-1-1-2 --pos 7680x-850 --rotate left
 notify-send "Display: WORK"

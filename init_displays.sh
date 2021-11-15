38fd5bc9572fd9aa0c1b9cf6fd68eed830ca33ea
Patrick Prell <prelly95@gmail.com>
2021-11-15 13:36:51 +1100

    fixinf screen layout

--- init_displays.sh
+++ init_displays.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sleep 1s
 disp_info=$(xrandr)
 disconnected=($(printf "$disp_info" | (grep "\bdisconnected\b") | awk '{print $1;}'))
 connected=($(printf "$disp_info" | grep "\bconnected\b" | awk '{print $1;}'))
@@ -8,10 +9,7 @@ disp_count=$(echo ${connected[*]} | wc -w)
 for dis in "${disconnected[@]}"; do
 	xrandr --output $dis --off
 done
-sleep 1s
-for con in "${connected[@]}"; do
-	xrandr --output $con --off
-done
+
 if [ "$disp_count" -eq "1" ]; then
 	/home/pat/scripts/screenlayout/laptop.sh ${connected[@]}
 elif [ "$disp_count" -eq "2" ]; then
--- screenlayout/work-setup.sh
+++ screenlayout/work-setup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 xrandr \
---output $1  --mode 3840x2160 -r 60 --crtc 1
+--output $1 --mode 3840x2160 --pos 0x0			-r 60	--rotate normal	--crtc 1 \
+--output $2 --mode 3840x2160 --pos 3840x0		-r 30	--rotate normal	--crtc 0 \
+--output $3 --mode 3840x2160 --pos 7680x-850	-r 30	--rotate left	--crtc 2
 
-xrandr \
---output $2 --mode 3840x2160 -r 30 --crtc 0 \
---output $3 --mode 3840x2160 -r 30 --crtc 2
+# xrandr --output $1 --pos 0x0 \
+# --output $2 --pos 3840x0 --rotate normal  --primary \
+# --output $3 --pos 7680x-850 --rotate left
 
-xrandr --output eDP-1-1 --pos 0x0 \
---output DP-1-1-1 --pos 3840x0 --rotate normal  --primary \
---output DP-1-1-2 --pos 7680x-850 --rotate left
 notify-send "Display: WORK"

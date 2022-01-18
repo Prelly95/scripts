a7acb89d87a9709d190bff28d77e1fb799d18a8c
Patrick Prell <prelly95@gmail.com>
2022-01-18 23:17:27 +1100

    fix setup_environment

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,32 +1,32 @@
-# [[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
+[[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
 
-# # Display Variables
-# export EDITOR=code-insiders
-# export GDK_DPI_SCALE=0.4
-# export GDK_SCALE=2
-# export QT_QPA_PLATFORMTHEME=qt5ct
+# Display Variables
+export EDITOR=code-insiders
+export GDK_DPI_SCALE=0.4
+export GDK_SCALE=2
+export QT_QPA_PLATFORMTHEME=qt5ct
+
 t=$(date | awk '{print $5}')
 hm=$(echo $t | cut -d':' -f1)$(echo $t | cut -d':' -f2)
-
 if [[ $hm -ge 1930 ]]; then
-	sct 2000 && brightness 30
+	sct 3500 && brillo -S 20
 else
-	brightness 100 && sct
+	sct && brillo -S 100
 fi
 
-# prog_bin=/usr/bin
-# # programs to start
-# $prog_bin/dunst &
-# $prog_bin/polkit-dumb-agent &
-# $prog_bin/pipewire &
-# $prog_bin/pipewire-pulse &
-# $prog_bin/pipewire-media-session &
+prog_bin=/usr/bin
+# programs to start
+$prog_bin/dunst &
+$prog_bin/polkit-dumb-agent &
+$prog_bin/pipewire &
+$prog_bin/pipewire-pulse &
+$prog_bin/pipewire-media-session &
 
-# $HOME/scripts/xinput_setup.sh &
+$HOME/scripts/xinput_setup.sh &
 
-# # Setup screen
-# $HOME/scripts/init_displays.sh &
+# Setup screen
+$HOME/scripts/init_displays.sh &
 
-# # Start gnome keyring
-# eval $(gnome-keyring-daemon --start)
-# export SSH_AUTH_SOCK
+# Start gnome keyring
+eval $(gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK

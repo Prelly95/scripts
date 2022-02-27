23c91483d1d802f4b4c219031e04c29634f9ca59
Patrick Prell <prelly95@gmail.com>
2022-02-27 21:05:54 +1100

    fix the screenshot names

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,6 @@
 sel=$(slop -f "-i %i -g %g") > /dev/null
 name=$(date +"%Y-%m-%d-%M-%S")
-destination="$HOME/Pictures/Screenshots/$name\_shotgun.png"
+destination="$HOME/Pictures/Screenshots/${name}_shotgun.png"
 shotgun $sel $destination
 xclip -selection clipboard -t image/png -i $destination
 feh $destination

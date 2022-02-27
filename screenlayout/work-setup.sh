8ccd0d10186f4367ebe3157191390f91e20f029b
Patrick Prell <prelly95@gmail.com>
2022-02-28 09:08:43 +1100

    fixed screen positioning

--- screenlayout/work-setup.sh
+++ screenlayout/work-setup.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 xrandr \
---output $1 --mode 3840x2160 --pos 0x0			-r 60	--rotate normal	--crtc 0 \
---output $2 --mode 3840x2160 --pos 3840x0		-r 30	--rotate normal	--crtc 1 \
+--output $1 --off --mode 3840x2160 --pos 0x0	-r 60	--rotate normal	--crtc 0 \
+--output $2 --mode 3840x2160 --pos 3840x0		-r 30	--rotate normal	--crtc 1 --primary \
 --output $3 --mode 3840x2160 --pos 7680x-850	-r 30	--rotate left	--crtc 2
 
-# xrandr --output $1 --pos 0x0 \
-# --output $2 --pos 3840x0 --rotate normal  --primary \
-# --output $3 --pos 7680x-850 --rotate left
 
 notify-send "Display: WORK"

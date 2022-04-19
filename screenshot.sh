4e6ef038748d8d33c0adc2a0222829137a0acd16
Patrick Prell <prelly95@gmail.com>
2022-04-19 10:55:39 +1000

    add debtap and small changes

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,7 @@
-sel=$(slop -f "-i %i -g %g") > /dev/null
+sel=$(slop -f "-i %i -g %g" -o) > /dev/null
+echo $sel
 name=$(date +"%Y-%m-%d-%M-%S")
 destination="$HOME/Pictures/Screenshots/${name}_shotgun.png"
 shotgun $sel $destination
 xclip -selection clipboard -t image/png -i $destination
-feh $destination
+feh -d --start-at $destination "$HOME/Pictures/Screenshots/"

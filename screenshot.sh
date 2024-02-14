c603958f7e4440192eee4bfa585041bd6f7057e7
Patrick Prell <patrick.prell@droneshield.com>
2024-02-14 16:31:33 +1100

    handles escape key more gracefully, only previews one image

--- screenshot.sh
+++ screenshot.sh
@@ -1,7 +1,16 @@
-sel=$(slop -f "-i %i -g %g" -o) > /dev/null
-echo $sel
+#!/bin/bash
+
+sel_str=$(slop -f "-i %i -g %g" -o) > /dev/null
+
+IFS=' ' read -ra sel <<< "$sel_str"
+
+if [[ ${#sel[@]} -eq 0 ]]; then
+    exit 1
+fi
+
 name=$(date +"%Y-%m-%d-%M-%S")
 destination="$HOME/Pictures/Screenshots/${name}_shotgun.png"
-shotgun $sel $destination
-xclip -selection clipboard -t image/png -i $destination
-feh -d --start-at $destination "$HOME/Pictures/Screenshots/"
+
+shotgun "${sel[@]}" "$destination"
+xclip -selection clipboard -t image/png -i "$destination"
+feh -d "$destination"

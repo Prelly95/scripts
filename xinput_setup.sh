679c2e5da76ed843992cbc1a088b8e94e565c665
Patrick Prell <patrick.prell@droneshield.com>
2024-09-10 09:44:38 +1000

    move set caps lock to top'

--- xinput_setup.sh
+++ xinput_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 xinput --set-prop "VEN_04F3:00 04F3:32AA Touchpad" "libinput Horizontal Scroll Enabled" 1
 xinput --set-prop "VEN_04F3:00 04F3:32AA Touchpad" "libinput Tapping Enabled" 1
 xinput --set-prop "VEN_04F3:00 04F3:32AA Touchpad" "libinput Tapping Button Mapping Enabled" 1 0
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 # /home/patrick/Tools/scripts/set_mouse_speed.sh
